19c1c812859118d55b0a609fa743c0fdfdc2c12e
student <student@kr.step>
2021-05-24 19:07:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      currentValue: 0
+    };
+  }
+
+  render(){
+    const handlerClick=()=>{
+      this.setState({currentValue: this.state.currentValue + 1});
+    };
+    return <button onClick={handlerClick}>
+      {this.state.currentValue}
+    </button>;
+  }
 }
 
-export default App;
+// export default class App extends React.Component{
+//   render(){
+//     return(
+//       <>
+//       <Counter/>
+//       </>
+//     );
+//   }
+// }
+
+export default function App(props) {
+  return(
+    <div className="App">
+      <h1>Information about writer:</h1>
+      <h2>{props.name}</h2>
+      <h2>{props.lastName}</h2>
+    </div>
+  )
+}
+
+App.defaultProps={name: "William", lastName: "Shakespeare"};
